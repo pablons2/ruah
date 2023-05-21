e2afbf6ed56d732cb194613774c84cdfd1834ec0
pablons2 <pablonicholas2012@gmail.com>
2023-05-21 11:11:30 -0300

    add | js for button

---
+++ ruah/js/js.js
@@ -0,0 +1,20 @@
+// Selecionar o botão pelo seu ID
+const botao = document.getElementById('enviar');
+
+// Adicionar um ouvinte de evento para o clique no botão
+botao.addEventListener('click', function() {
+  if(window.location.href = "https://formsubmit.co/mruah6@gmail.com"){
+
+  setTimeout(function() {
+   
+    window.location.href = 'index.html';
+  }, 3000);
+
+  }
+  
+});
+
+
+
+
+
